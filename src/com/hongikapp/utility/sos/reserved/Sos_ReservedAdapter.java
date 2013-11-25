c74b14e2364048f1a58f4f27ccff85e38ffc045e
Go JY <guiprog@nate.com>
2013-11-26 00:29:16 +0900

    commit check

--- src/com/hongikapp/utility/sos/reserved/Sos_ReservedAdapter.java
+++ src/com/hongikapp/utility/sos/reserved/Sos_ReservedAdapter.java
@@ -3,6 +3,7 @@ package com.hongikapp.utility.sos.reserved;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,22 +20,22 @@ public class Sos_ReservedAdapter  extends ArrayAdapter<Sos_Reserved_Data> {
 		super(context, 0, object);
 		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-	// hi hello nihao
-	// hihihiii
 	}
 
 	public View gespaceiew(int position, View v, ViewGroup parent) {
 		View view = null;
+		Log.d("nimi","sibal");
 		if (v == null) {
 			view = mInflater.inflate(R.layout.sos_reserved_info, null);
 		} else {
 			view = v;
 		}
+		Log.d("nimi","sibal");
 		// 자료를 받는다
 		final Sos_Reserved_Data data = this.getItem(position);
 		if (data != null) {
 			// 화면 출력
-			ImageView spaceimg = (ImageView) view.findViewById(R.id.imageView_mark);
+			
 			TextView space = (TextView) view.findViewById(R.id.txt_spaceName);
 			TextView date = (TextView) view.findViewById(R.id.txt_reservedDate);
 			TextView time = (TextView) view.findViewById(R.id.txt_reservedTime);
@@ -44,7 +45,6 @@ public class Sos_ReservedAdapter  extends ArrayAdapter<Sos_Reserved_Data> {
 			// ImageView에 뿌려질 해당 이미지값을 연결 3번째 인수값
 			// Texspaceiew1에 getLabel()을 출력 즉 1번째 인수값
 			//spaceimg.setImageURI(uri);
-			spaceimg.setImageResource(R.drawable.ic_launcher);
 			space.setText(data.getReserved_Space());
 			// Texspaceiew2에 getLabel()을 출력 즉 2번째 인수값
 			date.setText(data.getReserved_Date());
--- src/com/hongikapp/utility/sos/reserved/Sos_Reserved_main.java
+++ src/com/hongikapp/utility/sos/reserved/Sos_Reserved_main.java
@@ -3,12 +3,16 @@ package com.hongikapp.utility.sos.reserved;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.hongikapp.utility.sos.reservingin.Sos_TabHost_ReservingIn;
 import com.sos.R;
 
 public class Sos_Reserved_main extends Activity {
@@ -17,7 +21,7 @@ public class Sos_Reserved_main extends Activity {
 	private TextView userInfo;
 	Sos_ReservedAdapter Reserved_adapter; // Data를 연결할 Reserved_adapter
 	ArrayList<Sos_Reserved_Data> alist; // Data를 담을 자료구조
-	Button btnMore, btnCancel;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,13 +32,12 @@ public class Sos_Reserved_main extends Activity {
 		listView = (ListView) findViewById(R.id.txtlist_sos_reserved_list);
 		userInfo = (TextView) findViewById(R.id.txt_sos_reserved_title);
 		userInfo.setText("B011128 김현아 짱짱짱짱");
-		//btnMore = (Button) this.findViewById(R.id.btn_more);
-		//btnCancel = (Button) this.findViewById(R.id.btn_cancel);
-
+		Log.d("nimi1","sibal");
 		alist = new ArrayList<Sos_Reserved_Data>(); // Data를 연결할 Reserved_adapter
-		Reserved_adapter = new Sos_ReservedAdapter(this.getParent().getParent(), alist); // 데이터를 받기 위해 데이터어댑터 객체 선언
+		Log.d("nimi2","sibal");
+		Reserved_adapter = new Sos_ReservedAdapter(this.getParent(), alist); // 데이터를 받기 위해 데이터어댑터 객체 선언
 		// 리스트뷰에 어댑터 연결
-
+		Log.d("nimi3","sibal");
 		userInfo.setText("B011128 김현아 짱짱짱짱");
 
 		Reserved_adapter.add(new Sos_Reserved_Data(getApplicationContext(), "HOLLYS\nCOFFEE",
@@ -43,36 +46,11 @@ public class Sos_Reserved_main extends Activity {
 				"5월 20일\n7시에 예약되셨습니다.", R.drawable.ic_launcher));
 		Reserved_adapter.add(new Sos_Reserved_Data(getApplicationContext(), "CAFE\nBENEE",
 				"9월 20일\n3시에 예약되셨습니다.", R.drawable.ic_launcher));
-	
+		Log.d("nimi4","sibal");
 		listView.setAdapter(Reserved_adapter);
-		
-		//btnMore.setOnClickListener(mClick);
-		//btnCancel.setOnClickListener(mClick);
+		Log.d("nimi5","sibal");
 
 	}
 
-/*
-	OnClickListener mClick = new OnClickListener() {
 
-		@SuppressWarnings("deprecation")
-		public void onClick(View v) {
-			if (v == btnMore) {
-				Intent intent = new Intent(
-						Sos_Reserved_main.this,
-						com.hongikapp.utility.sos.reservingin.Sos_ReservingInSelectSpace.class);
-				Sos_TabHost_ReservingIn.ReservingIn_Group
-						.replaceView(Sos_TabHost_ReservingIn.ReservingIn_Group
-								.getLocalActivityManager()
-								.startActivity(
-										"Utility_Sos_reserved",
-										intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-								.getDecorView());
-			} else if (v == btnCancel) {
-				// 삭제할 수 있도록
-				//Reserved_adapter.delete(new Sos_Reserved_Data(getApplicationContext(), "CAFE\nBENE",
-				//		"9월 20일\n3시에 예약되셨습니다.", R.drawable.ic_launcher));
-				//
-			}
-		}
-	};*/
 }
